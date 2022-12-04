2f5b3a40cd5e32618e273686a9d0a6e3e784e059
shotaIDE <ide.shota.dev@gmail.com>
2022-12-05 00:47:44 +0900

    Revert "fix: CI動作確認のためのダミー修正"
    This reverts commit 4832bf86e7bfba4f77b237ffbe1680cdc5a3ef5d.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,8 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const test = 'test';
-
     return MaterialApp(
       title: 'Live Bresto',
       theme: ThemeData(
