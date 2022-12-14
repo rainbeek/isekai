907e658697df09091d1c1a398f611efd83c021e7
shotaIDE <ide.shota.dev@gmail.com>
2022-12-15 02:01:01 +0900

    fix: Firestoreをemulatorに接続

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,7 @@ Future<void> main() async {
   if (AppMode.serverEnv == ServerEnv.emulator) {
     const firebaseEmulatorHost = EnvironmentConfig.firebaseEmulatorHost;
     await FirebaseAuth.instance.useAuthEmulator(firebaseEmulatorHost, 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator(firebaseEmulatorHost, 8080);
   }
 
   runApp(
