e615f497fe9091840d98cc3ba193087f07530ec2
shotaIDE <ide.shota.dev@gmail.com>
2023-09-06 22:15:53 +0900

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:live_bresto/data/service/auth_service.dart';
+import 'package:live_bresto/data/usecase/session_use_case.dart';
 import 'package:live_bresto/ui/root_app.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockSessionActions extends Mock implements SessionActions {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (tester) async {
+  late MockSessionActions sessionActions;
+
+  setUp(() {
+    sessionActions = MockSessionActions();
+  });
+
+  testWidgets('ゲーム画面が正常に表示されること', (tester) async {
+    when(sessionActions.ensureLoggedIn).thenAnswer((_) async {});
+
     await tester.pumpWidget(
-      const ProviderScope(
+      ProviderScope(
         overrides: [
-          authActionsProvider.overrideWithValue(value),
+          sessionActionsProvider.overrideWithValue(sessionActions),
         ],
         child: RootApp(),
       ),
