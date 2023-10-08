8753b83f354104d2ca148b2a37dfac6f59c3494e
Shota Ide <ide.shota.dev@gmail.com>
2023-10-08 12:15:24 +0900

    fix: auto generated

---
+++ generator/ProfileScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "This is the profile screen",
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/thread_screen.dart
+++ lib/ui/thread_screen.dart
@@ -5,13 +5,23 @@ import 'package:live_bresto/data/model/thread.dart';
 import 'package:live_bresto/data/usecase/message_use_case.dart';
 import 'package:live_bresto/data/usecase/thread_use_case.dart';
 import 'package:live_bresto/ui/thread_presenter.dart';
+import 'ProfileScreen.dart'; // Import ProfileScreen
 
 final _threadPresenterProvider = Provider(
   (ref) => ThreadPresenter(messageActions: ref.watch(messageActionsProvider)),
 );
 
+
 class ThreadScreen extends ConsumerWidget {
   const ThreadScreen({super.key});
+
+  // Function to navigate to ProfileScreen
+  void _navigateToProfile(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ProfileScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -41,6 +51,12 @@ class ThreadScreen extends ConsumerWidget {
             return Text(thread.title);
           },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () => _navigateToProfile(context), // Navigate on press
+          ),
+        ],
       ),
       body: Column(
         children: [
