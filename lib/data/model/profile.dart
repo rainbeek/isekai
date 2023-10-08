18a0ee522514a620583b62dec59153b3a2357a5a
shotaIDE <ide.shota.dev@gmail.com>
2023-10-08 17:39:19 +0900

    fix

--- lib/data/model/profile.dart
+++ lib/data/model/profile.dart
@@ -1,5 +1,11 @@
-class Profile {
-  Profile({required this.icon, required this.name});
-  final String icon;
-  final String name;
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'profile.freezed.dart';
+
+@freezed
+class Profile with _$Profile {
+  const factory Profile({
+    required String icon,
+    required String name,
+  }) = _Profile;
 }
