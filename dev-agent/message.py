716de60202d4ca589d2c62fe1d6dcf63a43ccacd
shotaIDE <ide.shota.dev@gmail.com>
2023-10-08 19:11:06 +0900

    refactor

--- dev-agent/message.py
+++ dev-agent/message.py
@@ -1,8 +1,23 @@
 # coding: utf-8
 
+from enum import Enum
 from typing import Any, Dict, List
 
 import tiktoken
+
+
+class Role(Enum):
+    SYSTEM = 'system'
+    USER = 'user'
+    ASSISTANT = 'assistant'
+    FUNCTION = 'function'
+
+
+class MetaData:
+    def __init__(self, index: int, token: int, role: Role) -> None:
+        self.index = index
+        self.token = token
+        self.role = role
 
 
 class LlmMessageContainer:
@@ -12,53 +27,76 @@ class LlmMessageContainer:
         self.token_encoder = tiktoken.get_encoding("cl100k_base")
 
     def add_system_message(self, content: str) -> None:
-        self.meta_data_list.append({
-            "index": len(self.meta_data_list),
-            "token": len(self.token_encoder.encode(content)),
-            "role": "system"
+        self.meta_data_list.append(
+            MetaData(
+                index=len(self.meta_data_list),
+                token=len(self.token_encoder.encode(content)),
+                role=Role.SYSTEM
+            )
+        )
+        self.messages.append({
+            "role": Role.SYSTEM.value,
+            "content": content
         })
-        self.messages.append({"role": "system", "content": content})
 
     def add_user_message(self, content: str) -> None:
-        self.meta_data_list.append({
-            "index": len(self.meta_data_list),
-            "token": len(self.token_encoder.encode(content)),
-            "role": "user"
+        self.meta_data_list.append(
+            MetaData(
+                index=len(self.meta_data_list),
+                token=len(self.token_encoder.encode(content)),
+                role=Role.USER
+            )
+        )
+        self.messages.append({
+            "role": Role.USER.value,
+            "content": content
         })
-        self.messages.append({"role": "user", "content": content})
 
     def add_raw_message(self, message: Dict[str, Any]) -> None:
-        self.meta_data_list.append({
-            "index": len(self.meta_data_list),
-            "token": len(self.token_encoder.encode(message["content"])),
-            "role": message["role"]
+        content = message["content"]
+        token = \
+            len(self.token_encoder.encode(content)) if content is not None \
+            else 0
+        self.meta_data_list.append(
+            MetaData(
+                index=len(self.meta_data_list),
+                token=token,
+                role=Role(message["role"])
+            )
+        )
+        self.messages.append({
+            "role": Role(message["role"]).value,
+            "content": content
         })
-        self.messages.append(message)
 
     def total_token(self) -> int:
-        return sum(meta["token"] for meta in self.meta_data_list)
-
-    def add_raw_messages(self, messages: List[Dict[str, Any]]) -> None:
-        for message in messages:
-            self.add_raw_message(message)
+        return sum(meta.token for meta in self.meta_data_list)
 
     def to_capped_messages(self, token_limit: int = 28000) -> List[Dict[str, Any]]:
         if self.total_token() > token_limit:
-            system_metas = [
-                meta for meta in self.meta_data_list if meta["role"] == "system"]
-            not_system_metas = [
-                meta for meta in self.meta_data_list if meta["role"] != "system"]
-            current_token = sum(meta["token"] for meta in system_metas)
-            filtered_indexes = [meta["index"] for meta in system_metas]
+            system_meta_data_list = [
+                meta
+                for meta in self.meta_data_list
+                if meta.role == Role.SYSTEM
+            ]
+            not_system_meta_data_list = [
+                meta
+                for meta in self.meta_data_list
+                if meta.role != Role.SYSTEM]
+            current_token = sum(meta.token for meta in system_meta_data_list)
+            filtered_indexes = [meta.index for meta in system_meta_data_list]
 
-            for meta in reversed(not_system_metas):
-                if current_token + meta["token"] > token_limit:
+            for meta in reversed(not_system_meta_data_list):
+                if current_token + meta.token > token_limit:
                     break
-                current_token += meta["token"]
-                filtered_indexes.append(meta["index"])
+                current_token += meta.token
+                filtered_indexes.append(meta.index)
 
-            system_and_filtered_message = [message for i, message in enumerate(
-                self.messages) if i in filtered_indexes]
+            system_and_filtered_message = [
+                message
+                for i, message in enumerate(self.messages)
+                if i in filtered_indexes
+            ]
             return system_and_filtered_message
         else:
             return self.messages
