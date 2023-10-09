287d014fe29c545ff448f218a930fbb5a9f9c7b8
shotaIDE <ide.shota.dev@gmail.com>
2023-10-09 22:00:28 +0900

    fix

--- lib/data/usecase/preference_use_case.dart
+++ lib/data/usecase/preference_use_case.dart
@@ -54,7 +54,7 @@ class PreferenceActions {
   Profile _generateRandomProfile() {
     final icon = _generateRandomIcon();
     final name = _generateRandomName();
-    final validUntil = DateTime.now().add(const Duration(seconds: 30));
+    final validUntil = DateTime.now().add(const Duration(days: 1));
 
     return Profile(
       icon: icon,
