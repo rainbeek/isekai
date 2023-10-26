84da6545c26287e3e40085344b39d70a70d9c338
shotaIDE <ide.shota.dev@gmail.com>
2023-10-26 22:16:57 +0900

    fix

--- lib/data/usecase/message_use_case.dart
+++ lib/data/usecase/message_use_case.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer-match-file-name
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isekai/data/definition/app_mode.dart';
 import 'package:isekai/data/service/database_service.dart';
