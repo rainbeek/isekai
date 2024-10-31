f22ceca814295010413d608c6113b9a4f763abd9
shotaIDE <ide.shota.dev@gmail.com>
2024-11-01 00:11:22 +0900

    fix

--- lib/ui/debug_screen.dart
+++ lib/ui/debug_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -24,6 +25,7 @@ class DebugScreen extends StatelessWidget {
         children: [
           const SizedBox(height: 16),
           const _UpdateProfileTile(),
+          const _ForceErrorTile(),
           const _ForceCrashTile(),
           SizedBox(height: MediaQuery.of(context).viewPadding.bottom + 16),
         ],
@@ -60,6 +62,18 @@ class _UpdateProfileTile extends ConsumerWidget {
   }
 }
 
+class _ForceErrorTile extends StatelessWidget {
+  const _ForceErrorTile();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('強制エラー'),
+      onTap: () => throw Exception('Force error on debug screen'),
+    );
+  }
+}
+
 class _ForceCrashTile extends StatelessWidget {
   const _ForceCrashTile();
 
@@ -67,7 +81,7 @@ class _ForceCrashTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: const Text('強制クラッシュ'),
-      onTap: () => throw Exception('Force crash on debug screen'),
+      onTap: () => FirebaseCrashlytics.instance.crash(),
     );
   }
 }
