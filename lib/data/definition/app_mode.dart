00e738c4d71e06f8eb37bebe0feb6c9d3906febe
Shota Ide <ide.shota.dev@gmail.com>
2024-12-16 22:27:12 +0900

    Revert "Update app_mode.dart"
    This reverts commit 28698a4e07837c667bfcfb3309dd1dc811d22fbe.

--- lib/data/definition/app_mode.dart
+++ lib/data/definition/app_mode.dart
@@ -13,7 +13,6 @@ const threadForDebug = Thread(title: 'Default thread for debugging');
 
 ServerEnv _getApiEnv() {
   const serverEnvString = String.fromEnvironment('FLAVOR');
-  print('ServerEnv: $serverEnvString');
 
   return ServerEnv.values.firstWhere(
     (value) => value.name == serverEnvString,
